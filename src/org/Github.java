d083f47dbd6eca8dabc457591ae14d99339999aa
arunkarthick69 <ARUNKARTHICK696@gmail.com>
2022-11-24 22:29:19 +0530

    sachin update the coding

--- src/org/Github.java
+++ src/org/Github.java
@@ -5,6 +5,10 @@ public class Github {
 	public static void main(String[] args) {
 		int a =100;
 		System.out.println(a);
+		int b =200;
+		System.out.println(b);
+		int c =300;
+		System.out.println(c);
 	}
 	
 	
